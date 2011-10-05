c75f22eeb0f350dd0e6278d373b3baa2e50e1cd3
Michael van Tellingen <michaelvantellingen@gmail.com>
2011-10-05 18:02:19 +0200

    Import the various dbapi 2.0 types at module level

--- psycopg2ct/__init__.py
+++ psycopg2ct/__init__.py
@@ -4,8 +4,8 @@ from time import localtime
 from psycopg2ct.exceptions import *
 from psycopg2ct import extensions
 from psycopg2ct.connection import connect
-from psycopg2ct.extensions import Binary, STRING
-
+from psycopg2ct.extensions import Binary
+from psycopg2ct.extensions import BINARY, DATETIME, NUMBER, ROWID, STRING
 
 __version__ = '2.4'
 apilevel = '2.0'
