cf3b0e6b648f08cd3f616e0da679af7a038c2e1b
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2011-10-05 22:01:32 +0100

    Implemented DBAPI time-related module level function

--- psycopg2ct/__init__.py
+++ psycopg2ct/__init__.py
@@ -6,6 +6,7 @@ from psycopg2ct.extensions import Binary
 from psycopg2ct.extensions import BINARY, DATETIME, NUMBER, ROWID, STRING
 from psycopg2ct._impl.connection import connect
 from psycopg2ct._impl.exceptions import *
+from psycopg2ct.tz import LOCAL as _LOCAL
 
 __version__ = '2.4'
 apilevel = '2.0'
@@ -16,10 +17,24 @@ def Date(year, month, day):
     date = datetime.date(year, month, day)
     return extensions.DateTime(date)
 
+def Time(hour, minutes, seconds, tzinfo=None):
+    time = datetime.time(hour, minutes, seconds, tzinfo=tzinfo)
+    return extensions.DateTime(time)
+
+def Timestamp(year, month, day, hour, minutes, seconds, tzinfo=None):
+    dt = datetime.datetime(
+        year, month, day, hour, minutes, seconds, tzinfo=tzinfo)
+    return extensions.DateTime(dt)
 
 def DateFromTicks(ticks):
-    """FIXME: ?"""
-    tm = localtime()
-    return Date(tm.tm_year, tm.tm_mon, tm.tm_mday)
+    date = datetime.datetime.fromtimestamp(ticks).date()
+    return extensions.DateTime(date)
 
+def TimeFromTicks(ticks):
+    time = datetime.datetime.fromtimestamp(ticks).time()
+    return extensions.DateTime(time)
+
+def TimestampFromTicks(ticks):
+    dt = datetime.datetime.fromtimestamp(ticks, _LOCAL)
+    return extensions.DateTime(dt)
 
