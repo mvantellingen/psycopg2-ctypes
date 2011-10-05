d651f8541b0752813473235c5273032315916666
Michael van Tellingen <michaelvantellingen@gmail.com>
2011-10-05 17:52:19 +0200

    Add the compat module

---
+++ psycopg2ct/tests/psycopg2_tests/psycopg2.py
@@ -0,0 +1,10 @@
+import sys, os.path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+from psycopg2ct import compat
+
+compat.register()
+
+
+# Hack to make py.test and nose work
+sys.modules['psycopg2ct.tests.psycopg2_tests.psycopg2'] = sys.modules['psycopg2']
