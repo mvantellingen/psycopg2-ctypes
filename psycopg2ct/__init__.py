3bf543f78fc05d6db557f05b77531ea1a548bba0
Michael van Tellingen <michaelvantellingen@gmail.com>
2011-10-15 16:07:11 +0200

    See previous commit

--- psycopg2ct/__init__.py
+++ psycopg2ct/__init__.py
@@ -2,13 +2,13 @@ import datetime
 from time import localtime
 
 from psycopg2ct import extensions
-from psycopg2ct.extensions import Binary
-from psycopg2ct.extensions import BINARY, DATETIME, NUMBER, ROWID, STRING
+from psycopg2ct import tz
+from psycopg2ct._impl.adapters import Binary, Date, Time, Timestamp
+from psycopg2ct._impl.adapters import DateFromTicks, TimeFromTicks
+from psycopg2ct._impl.adapters import TimestampFromTicks
 from psycopg2ct._impl.connection import connect
 from psycopg2ct._impl.exceptions import *
-from psycopg2ct.tz import LOCAL as _LOCAL
-
-from psycopg2ct import tz
+from psycopg2ct._impl.typecasts import BINARY, DATETIME, NUMBER, ROWID, STRING
 
 __version__ = '2.4'
 apilevel = '2.0'
@@ -23,3 +23,5 @@ threadsafety = 1
 import psycopg2ct.extensions as _ext
 _ext.register_adapter(tuple, _ext.SQL_IN)
 _ext.register_adapter(type(None), _ext.NoneAdapter)
+
+__all__ = filter(lambda k: not k.startswith('_'), locals().keys())
