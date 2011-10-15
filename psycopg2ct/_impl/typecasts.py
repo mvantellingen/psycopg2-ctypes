2320340a56d89655189bd4cbdf6a336023dee09b
Michael van Tellingen <michaelvantellingen@gmail.com>
2011-10-15 16:06:59 +0200

    Move code from extensions to _impl.typecasts and add various array types.
    It now matches the psycopg2.extensions

--- psycopg2ct/_impl/typecasts.py
+++ psycopg2ct/_impl/typecasts.py
@@ -8,6 +8,50 @@ from psycopg2ct._impl import libpq
 
 string_types = {}
 
+binary_types = {}
+
+
+class Type(object):
+    def __init__(self, name, values, caster=None, py_caster=None):
+        self.name = name
+        self.values = values
+        self.caster = caster
+        self.py_caster = py_caster
+
+    def __eq__(self, other):
+        return other in self.values
+
+    def cast(self, value, cursor, length=None):
+        if self.py_caster is not None:
+            return self.py_caster(value, cursor)
+        return self.caster(value, length, cursor)
+
+
+def register_type(type_obj, scope=None):
+    typecasts = string_types
+    if scope:
+        from psycopg2ct._impl.connection import Connection
+        from psycopg2ct._impl.cursor import Cursor
+
+        if isinstance(scope, Connection):
+            typecasts = scope._typecasts
+        elif isinstance(scope, Cursor):
+            typecasts = scope._typecasts
+        else:
+            typecasts = None
+
+    for value in type_obj.values:
+        typecasts[value] = type_obj
+
+
+def new_type(values, name, castobj):
+    return Type(name, values, py_caster=castobj)
+
+
+def new_array_type(values, name, baseobj):
+    caster = parse_array(baseobj)
+    return Type(name, values, caster=caster)
+
 
 def typecast(caster, value, length, cursor):
     return caster.cast(value, cursor, length)
@@ -256,3 +300,60 @@ def DateFromTicks(ticks):
 def Binary(obj):
     from psycopg2ct.extensions.adapters import Binary
     return Binary(obj)
+
+
+def _default_type(name, oids, caster):
+    """Shortcut to register internal types"""
+    type_obj = Type(name, oids, caster)
+    register_type(type_obj)
+    return type_obj
+
+
+# DB API 2.0 types
+BINARY = _default_type('BINARY', [17], parse_binary)
+DATETIME = _default_type('DATETIME',  [1114, 1184, 704, 1186], parse_datetime)
+NUMBER = _default_type('NUMBER', [20, 33, 21, 701, 700, 1700], parse_float)
+ROWID = _default_type('ROWID', [26], parse_integer)
+STRING = _default_type('STRING', [19, 18, 25, 1042, 1043], parse_string)
+
+# Register the basic typecasters
+BOOLEAN = _default_type('BOOLEAN', [16], parse_boolean)
+DATE = _default_type('DATE', [1082], parse_date)
+DECIMAL = _default_type('DECIMAL', [1700], parse_decimal)
+FLOAT = _default_type('FLOAT', [701, 700], parse_float)
+INTEGER = _default_type('INTEGER', [23, 21], parse_integer)
+INTERVAL = _default_type('INTERVAL', [704, 1186], parse_interval)
+LONGINTEGER = _default_type('LONGINTEGER', [20], parse_longinteger)
+TIME = _default_type('TIME', [1083, 1266], parse_time)
+UNKNOWN = _default_type('UNKNOWN', [705], parse_unknown)
+
+# Array types
+BINARYARRAY = _default_type(
+    'BINARYARRAY', [1001], parse_array(BINARY))
+BOOLEANARRAY = _default_type(
+    'BOOLEANARRAY', [1000], parse_array(BOOLEAN))
+DATEARRAY = _default_type(
+    'DATEARRAY', [1182], parse_array(DATE))
+DATETIMEARRAY = _default_type(
+    'DATETIMEARRAY', [1115, 1185], parse_array(DATETIME))
+DECIMALARRAY = _default_type(
+    'DECIMALARRAY', [1231], parse_array(DECIMAL))
+FLOATARRAY = _default_type(
+    'FLOATARRAY', [1017, 1021, 1022], parse_array(FLOAT))
+INTEGERARRAY = _default_type(
+    'INTEGERARRAY', [1005, 1006, 1007], parse_array(INTEGER))
+INTERVALARRAY = _default_type(
+    'INTERVALARRAY', [1187], parse_array(INTERVAL))
+LONGINTEGERARRAY = _default_type(
+    'LONGINTEGERARRAY', [1016], parse_array(LONGINTEGER))
+ROWIDARRAY = _default_type(
+    'ROWIDARRAY', [1013, 1028], parse_array(ROWID))
+STRINGARRAY = _default_type(
+    'STRINGARRAY', [1002, 1003, 1009, 1014, 1015], parse_array(STRING))
+TIMEARRAY = _default_type(
+    'TIMEARRAY', [1183, 1270], parse_array(TIME))
+
+
+UNICODE = Type('UNICODE', [19, 18, 25, 1042, 1043], parse_unicode)
+UNICODEARRAY = Type('UNICODEARRAY', [1002, 1003, 1009, 1014, 1015],
+    parse_array(UNICODE))
--- psycopg2ct/extensions.py
+++ psycopg2ct/extensions.py
@@ -1,31 +1,37 @@
-import datetime
-import decimal
-import math
+"""psycopg extensions to the DBAPI-2.0
 
-from psycopg2ct._config import PG_VERSION
-from psycopg2ct._impl import libpq
-from psycopg2ct._impl import typecasts
+This module holds all the extensions to the DBAPI-2.0 provided by psycopg.
+
+- `connection` -- the new-type inheritable connection class
+- `cursor` -- the new-type inheritable cursor class
+- `lobject` -- the new-type inheritable large object class
+- `adapt()` -- exposes the PEP-246_ compatible adapting mechanism used
+  by psycopg to adapt Python types to PostgreSQL ones
+
+.. _PEP-246: http://www.python.org/peps/pep-0246.html
+"""
+import sys as _sys
+
 from psycopg2ct._impl.adapters import adapt, adapters
+from psycopg2ct._impl.adapters import Binary, Boolean, Int, Float
+from psycopg2ct._impl.adapters import QuotedString, AsIs, ISQLQuote
 from psycopg2ct._impl.connection import Connection as connection
 from psycopg2ct._impl.consts import *
 from psycopg2ct._impl.cursor import Cursor as cursor
 from psycopg2ct._impl.encodings import encodings
-from psycopg2ct._impl.exceptions import ProgrammingError
 from psycopg2ct._impl.exceptions import QueryCanceledError
 from psycopg2ct._impl.exceptions import TransactionRollbackError
+from psycopg2ct._impl.notify import Notify
+from psycopg2ct._impl.typecasts import (
+    UNICODE, INTEGER, LONGINTEGER, BOOLEAN, FLOAT, TIME, DATE, INTERVAL,
+    DECIMAL,
+    BINARYARRAY, BOOLEANARRAY, DATEARRAY, DATETIMEARRAY, DECIMALARRAY,
+    FLOATARRAY, INTEGERARRAY, INTERVALARRAY, LONGINTEGERARRAY, ROWIDARRAY,
+    STRINGARRAY, TIMEARRAY, UNICODEARRAY)
+from psycopg2ct._impl.typecasts import string_types, binary_types
+from psycopg2ct._impl.typecasts import new_type, new_array_type, register_type
 from psycopg2ct._impl.xid import Xid
 
-
-from psycopg2ct._impl.adapters import Binary, Boolean, Int, Float
-from psycopg2ct._impl.adapters import QuotedString, AsIs, ISQLQuote
-
-
-from psycopg2ct._impl.notify import Notify
-
-# The following are not available in psycopg2.extensions so figure that out.
-from psycopg2ct._impl.adapters import List, DateTime, Decimal
-
-import sys as _sys
 
 # Return bytes from a string
 if _sys.version_info[0] < 3:
@@ -35,11 +41,9 @@ else:
     def b(s):
         return s.encode('utf8')
 
-string_types = typecasts.string_types
-
-
 
 def register_adapter(typ, callable):
+    """Register 'callable' as an ISQLQuote adapter for type 'typ'."""
     adapters[(typ, ISQLQuote)] = callable
 
 
@@ -80,85 +84,4 @@ class NoneAdapter(object):
         return _null
 
 
-class Type(object):
-    def __init__(self, name, values, caster=None, py_caster=None):
-        self.name = name
-        self.values = values
-        self.caster = caster
-        self.py_caster = py_caster
-
-    def __eq__(self, other):
-        return other in self.values
-
-    def cast(self, value, cursor, length=None):
-        if self.py_caster is not None:
-            return self.py_caster(value, cursor)
-        return self.caster(value, length, cursor)
-
-
-def register_type(type_obj, scope=None):
-    typecasts = string_types
-    if scope:
-        from psycopg2ct._impl.connection import Connection
-        from psycopg2ct._impl.cursor import Cursor
-
-        if isinstance(scope, Connection):
-            typecasts = scope._typecasts
-        elif isinstance(scope, Cursor):
-            typecasts = scope._typecasts
-        else:
-            typecasts = None
-
-    for value in type_obj.values:
-        typecasts[value] = type_obj
-
-
-def new_type(values, name, castobj):
-    return Type(name, values, py_caster=castobj)
-
-
-def new_array_type(values, name, baseobj):
-    caster = typecasts.parse_array(baseobj)
-    return Type(name, values, caster=caster)
-
-
-def _default_type(name, oids, caster):
-    """Shortcut to register internal types"""
-    type_obj = Type(name, oids, caster)
-    register_type(type_obj)
-    return type_obj
-
-# DB API 2.0 types
-BINARY = _default_type('BINARY', [17], typecasts.parse_binary)
-DATETIME = _default_type('DATETIME',  [1114, 1184, 704, 1186], typecasts.parse_datetime)
-NUMBER = _default_type('NUMBER', [20, 33, 21, 701, 700, 1700], typecasts.parse_float)
-ROWID = _default_type('ROWID', [26], typecasts.parse_integer)
-STRING = _default_type('STRING', [19, 18, 25, 1042, 1043], typecasts.parse_string)
-
-# Register the basic typecasters
-UNKNOWN = _default_type('UNKNOWN', [705], typecasts.parse_unknown)
-BOOLEAN = _default_type('BOOLEAN', [16], typecasts.parse_boolean)
-DATE = _default_type('DATE', [1082], typecasts.parse_date)
-DECIMAL = _default_type('DECIMAL', [1700], typecasts.parse_decimal)
-FLOAT = _default_type('FLOAT', [701, 700], typecasts.parse_float)
-INTEGER = _default_type('INTEGER', [23, 21], typecasts.parse_integer)
-INTERVAL = _default_type('INTERVAL', [704, 1186], typecasts.parse_interval)
-LONGINTEGER = _default_type('LONGINTEGER', [20], typecasts.parse_longinteger)
-TIME = _default_type('TIME', [1083, 1266], typecasts.parse_time)
-
-
-UNICODE = Type('UNICODE', [19, 18, 25, 1042, 1043], typecasts.parse_unicode)
-
-# Array types
-INTEGERARRAY = _default_type(
-    'INTEGERARRAY', [1005, 1006, 1007], typecasts.parse_array(INTEGER))
-FLOATARRAY = _default_type(
-    'FLOATARRAY', [1017, 1021, 1022], typecasts.parse_array(FLOAT))
-DECIMALARRAY = _default_type(
-    'DECIMALARRAY', [1231], typecasts.parse_array(DECIMAL))
-STRINGARRAY = _default_type(
-    'STRINGARRAY', [1002, 1003, 1009, 1014, 1015], typecasts.parse_array(STRING))
-BINARYARRAY = _default_type(
-    'BINARYARRAY', [1001], typecasts.parse_array(BINARY))
-DATETIMEARRAY = _default_type(
-    'DATETIMEARRAY', [1115, 1185], typecasts.parse_array(DATETIME))
+__all__ = filter(lambda k: not k.startswith('_'), locals().keys())
